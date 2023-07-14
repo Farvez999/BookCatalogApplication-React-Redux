5bfdc9af5715e242cdd53d3d32393468e00e0ddb
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-07-14 11:48:37 +0600

    shadcn ui setup done

--- client-site/src/App.tsx
+++ client-site/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default App;
